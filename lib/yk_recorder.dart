03415164d2b27a83d7836897d0d1ab166d0828aa
534272374 <534272374@qq.com>
2025-06-05 19:51:23 +0800

    update tools

--- lib/yk_recorder.dart
+++ lib/yk_recorder.dart
@@ -7,17 +7,11 @@ mixin YkRecorderDelegate {
 
   Future dispose();
 
-  Future<bool> start(
-      {required String toFilePath,
-        required void Function(Duration duration) progressCallBack,
-        required Future Function() endCallBack,
-        int maxSeconds = 0});
+  Future<bool> start(String toFilePath);
 
   Future end();
 
   Future<bool> checkGrant();
-
-  error(String msg);
 }
 
 class YkRecorderConfig with ChangeNotifier {
@@ -38,25 +32,22 @@ class YkRecorderConfig with ChangeNotifier {
   }
 }
 
-class YkRecorder {
-
+class YkRecorder with ChangeNotifier {
   YkRecorderDelegate? _delegate;
   bool _isRecording = false;
   void Function(String? path, Duration? duration)? _onCompleteCallBack;
   String? _currentFilePath;
-  Duration? _duration;
+  Timer? _timer;
+  int duration = 0;
+  bool _didDispose = false;
 
-
-  final _durationStreamController = StreamController<Duration?>.broadcast();
-
-  Stream<Duration?> get durationStream => _durationStreamController.stream;
-
-  Future setup({required YkRecorderDelegate delegate}) {
+  Future init({required YkRecorderDelegate delegate}) {
     _delegate = delegate;
     return delegate.init();
   }
 
-  Future<void> start({required String toFilePath, required void Function(String? path, Duration? duration) onCompleteCallBack, int maxSeconds = 0}) async {
+  Future<void> start(
+      {required String toFilePath, required void Function(String? path, Duration? duration) onCompleteCallBack, int maxSeconds = 0}) async {
     if (_delegate == null) {
       return Future.error('未注册 delegate');
     }
@@ -70,20 +61,25 @@ class YkRecorder {
       }
       _currentFilePath = toFilePath;
       _onCompleteCallBack = onCompleteCallBack;
-      final didRecord = await _delegate!.start(
-          toFilePath: toFilePath,
-          progressCallBack: (duration) {
-            _duration = duration;
-            _durationStreamController.add(duration);
-          },
-          endCallBack: () {
-            return end();
-          },
-          maxSeconds: maxSeconds);
+      final didRecord = await _delegate!.start(toFilePath);
+      _timer?.cancel();
+      if (didRecord) {
+        duration = 0;
+
+        _timer?.cancel();
+        _timer = Timer.periodic(const Duration(seconds: 1), (timer) async {
+          duration += 1;
+          if (duration >= maxSeconds) {
+            await end();
+          } else {
+            notifyListeners();
+          }
+        });
+      }
       _isRecording = didRecord;
       YkRecorderConfig.instance._changeRecord(recording: didRecord);
     } catch (e) {
-      _delegate?.error("_start ${e.toString()}");
+      // _delegate?.error("_start ${e.toString()}");
     }
     return Future.value();
   }
@@ -99,18 +95,28 @@ class YkRecorder {
     //结束录音逻辑
     try {
       await _delegate!.end();
+      _timer?.cancel();
       _isRecording = false;
       YkRecorderConfig.instance._changeRecord(recording: false);
-      _onCompleteCallBack?.call(_currentFilePath, _duration);
-      _duration = null;
-      _durationStreamController.add(null);
+      _onCompleteCallBack?.call(_currentFilePath, Duration(seconds: duration));
+      duration = 0;
+      if (!_didDispose) {
+        notifyListeners();
+      }
     } catch (e) {
-      _delegate?.error("_end ${e.toString()}");
+      // _delegate?.error("_end ${e.toString()}");
     }
     return Future.value("");
   }
 
-  dispose() async {
+  @override
+  void dispose() {
+    _didDispose = true;
+    _unInit();
+    super.dispose();
+  }
+
+  Future _unInit() async {
     if (_delegate != null) {
       await end();
       await _delegate!.dispose();
